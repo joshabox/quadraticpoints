2bc0f0011a596fc8c81e706ea9569f002b292180
joshabox <50105934+joshabox@users.noreply.github.com>
2020-01-13 17:38:10 +0000

    Update X053.m

--- X053.m
+++ X053.m
@@ -59,6 +59,7 @@ A:=AbelianGroup([0,13]);
 divs:=[D1,Dtor];
 genusC:=Genus(C);
 auts:=[al[1]];
+I:=2;
 load "quadptssieve.m";
 MWSieve(deg2,[11,7]); //Returns boolean true if we have indeed found all exc degree 2 pts.
 
